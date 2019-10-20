869926277c912c184ccbdfdfefab70b40ed4817d
Ahmed Lotfy <Ahmedlasx@gmail.com>
2019-10-20 20:41:37 +0200

    Fix imports, and add MainTabs to the home page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,39 @@
-import * as React from 'react';
+import React from 'react'
+import Layout from './components/Layout'
+import MainTabs from './components/MainTabs'
+
+const tabsData = [
+   {
+      label: 'Ok 1',
+      content: 'Content 1'
+   },
+   {
+      label: 'Ok 2',
+      content: 'Content 2'
+   },
+   {
+      label: 'Ok 3',
+      content: 'Content 3'
+   }
+]
 
 function App() {
    return (
-      <div>
-         <h1>Welcome to React</h1>
-         <h4> Also, you <i>shoudl</i> know that kinda hot patching works! &lt;= fix should and hit refresh</h4>
-         Advantages:
+      <Layout>
+         <MainTabs tabs={tabsData}></MainTabs>
+         <div>
+            <h1>Welcome to React</h1>
+            <h4> Also, you <i>shoudl</i> know that kinda hot patching works! &lt;= fix should and hit refresh</h4>
+            Advantages:
          <ul>
-            <li>BS Free.. 2 dependencies which are react and react-dom. </li>
-            <li>BS Free (continued).. 10 Dev dependencies.</li>
-            <li>Completely written in typescript.</li>
-            <li>Supporting JSX without Babel.. (continuation of being BS Free).</li>
-            <li> <text style={{ textDecorationLine: 'line-through', textDecorationStyle: 'solid' }}>
-               Hot</text> warm reloading :P. Edit then go to react and do ctrl + R </li>
-         </ul>
-      </div>
-   );
+               <li>BS Free.. 2 dependencies which are react and react-dom. </li>
+               <li>BS Free (continued).. 10 Dev dependencies.</li>
+               <li>Completely written in typescript.</li>
+               <li>Supporting JSX without Babel.. (continuation of being BS Free).</li>
+               <li> Hot warm reloading :P. Edit then go to react and do ctrl + R </li>
+            </ul>
+         </div>
+      </Layout>
+   )
 }
-export default App;
+export default App
